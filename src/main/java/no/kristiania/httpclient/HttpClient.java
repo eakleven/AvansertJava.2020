fb55811d733dfc94e2c257d015ace3150f247801
eakleven <54510623+eakleven@users.noreply.github.com>
2020-09-09 14:07:39 +0200

    shouldReadStatusCode - green

--- src/main/java/no/kristiania/httpclient/HttpClient.java
+++ src/main/java/no/kristiania/httpclient/HttpClient.java
@@ -4,24 +4,39 @@ import java.io.IOException;
 import java.net.Socket;
 
 public class HttpClient {
-    public HttpClient(String hostname, int port, String requestTarget) {
+    private final int responseCode;
+
+    public HttpClient(String hostname, int port, String requestTarget) throws IOException {
+        Socket socket = new Socket (hostname, port);
+
+        String request = "GET " + requestTarget + " HTTP/1.1\r\n" +
+                "Host: " + hostname + "\r\n\r\n";
+        socket.getOutputStream().write(request.getBytes());
+
+        StringBuilder line = new StringBuilder();
+        int c;
+        while ((c = socket.getInputStream().read()) != -1){
+            if (c=='\n'){
+                break;
+            }
+            line.append((char)c);
+        }
+        System.out.println(line);
+        String[] responseLineParts = line.toString().split(" ");
+        responseCode = Integer.parseInt(responseLineParts[1]);
 
     }
 
     public static void main(String[] args) throws IOException {
-        Socket socket = new Socket ("urlecho.appspot.com", 80);
+        String hostname = "urlecho.appspot.com";
+        int port = 80;
+        String requestTarget = "/echo?status=200&body=Hello%20world!";
+        new HttpClient(hostname, port, requestTarget);
 
-        String request = "GET /echo?status=200&body=Hello%20world! HTTP/1.1\n" +
-                "Host: urlecho.appspot.com\r\n\r\n";
-        socket.getOutputStream().write(request.getBytes());
 
-        int c;
-        while ((c = socket.getInputStream().read()) != -1){
-            System.out.print((char)c);
-        }
     }
 
     public int getResponseCode() {
-        return 200;
+        return responseCode;
     }
 }
