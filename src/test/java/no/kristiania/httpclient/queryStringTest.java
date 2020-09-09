e92e4cf32e9874f63ef3e8c41f5f0dcd8d4682a7
eakleven <54510623+eakleven@users.noreply.github.com>
2020-09-09 12:34:03 +0200

    Red test

--- src/test/java/no/kristiania/httpclient/queryStringTest.java
+++ src/test/java/no/kristiania/httpclient/queryStringTest.java
@@ -11,4 +11,10 @@ public class queryStringTest {
         assertEquals("200", queryString.getParameter("status"));
     }
 
+    @Test
+    void shouldRetrieveOtherQueryParameter(){
+        QueryString queryString = new QueryString("status-404");
+        assertEquals("404", queryString.getParameter("status"));
+    }
+
 }
