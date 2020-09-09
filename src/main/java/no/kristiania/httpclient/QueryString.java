a8cee917d69778ece184bc0f72131c7eb1ee86e5
eakleven <54510623+eakleven@users.noreply.github.com>
2020-09-09 13:08:13 +0200

    Complete parsing of queryString (#1)
    * Red test
    
    * Green for status, red for paramter
    
    * All tests green

--- src/main/java/no/kristiania/httpclient/QueryString.java
+++ src/main/java/no/kristiania/httpclient/QueryString.java
@@ -1,11 +1,26 @@
 package no.kristiania.httpclient;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class QueryString {
+    private Map<String, String> parameters = new HashMap<>();
+
+
     public QueryString(String queryString) {
+        for (String parameter : queryString.split("&")){
+            int equalPos = parameter.indexOf('=');
+            String value = parameter.substring(equalPos+1);
+            String parameterName = parameter.substring(0, equalPos);
+            parameters.put(parameterName, value);
+        }
 
     }
 
     public String getParameter(String name) {
-        return "200";
+        return parameters.get(name);
+
     }
+
 }
+
--- src/test/java/no/kristiania/httpclient/queryStringTest.java
+++ src/test/java/no/kristiania/httpclient/queryStringTest.java
@@ -7,8 +7,28 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class queryStringTest {
     @Test
     void shouldRetrieveQueryParameter(){
-        QueryString queryString = new QueryString("status-200");
+        QueryString queryString = new QueryString("status=200");
         assertEquals("200", queryString.getParameter("status"));
+    }
+
+    @Test
+    void shouldRetrieveOtherQueryParameter(){
+        QueryString queryString = new QueryString("status=404");
+        assertEquals("404", queryString.getParameter("status"));
+    }
+
+    @Test
+    void shouldRetrieveParameterByName(){
+        QueryString queryString = new QueryString("text=Hello");
+        assertEquals(null, queryString.getParameter("status"));
+        assertEquals("Hello", queryString.getParameter("text"));
+    }
+
+    @Test
+    void shouldHandleMultipleParameters(){
+        QueryString queryString = new QueryString("text=Hello&status=200");
+        assertEquals("200", queryString.getParameter("status"));
+        assertEquals("Hello", queryString.getParameter("text"));
     }
 
 }
