ea8dda42127d99b0456cc0b14e39bf75732e0ffa
eakleven <54510623+eakleven@users.noreply.github.com>
2020-09-09 14:49:57 +0200

    Read response headers

--- src/main/java/no/kristiania/httpclient/HttpClient.java
+++ src/main/java/no/kristiania/httpclient/HttpClient.java
@@ -2,9 +2,12 @@ package no.kristiania.httpclient;
 
 import java.io.IOException;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
 
 public class HttpClient {
     private final int responseCode;
+    private Map<String, String> responseHeaders = new HashMap<>();
 
     public HttpClient(String hostname, int port, String requestTarget) throws IOException {
         Socket socket = new Socket (hostname, port);
@@ -13,18 +16,33 @@ public class HttpClient {
                 "Host: " + hostname + "\r\n\r\n";
         socket.getOutputStream().write(request.getBytes());
 
+        String line = readLine(socket);
+        System.out.println(line);
+        String[] responseLineParts = line.split(" ");
+        responseCode = Integer.parseInt(responseLineParts[1]);
+
+        String headerLine;
+        while (!(headerLine = readLine(socket)).isEmpty()){
+            System.out.println(headerLine);
+            int colonPos = headerLine.indexOf(':');
+            String name = headerLine.substring(0, colonPos);
+            String value = headerLine.substring(colonPos+1).trim();
+            responseHeaders.put(name, value);
+        }
+
+    }
+
+    private String readLine(Socket socket) throws IOException {
         StringBuilder line = new StringBuilder();
         int c;
         while ((c = socket.getInputStream().read()) != -1){
-            if (c=='\n'){
+            if (c=='\r'){
+                socket.getInputStream().read();
                 break;
             }
             line.append((char)c);
         }
-        System.out.println(line);
-        String[] responseLineParts = line.toString().split(" ");
-        responseCode = Integer.parseInt(responseLineParts[1]);
-
+        return line.toString();
     }
 
     public static void main(String[] args) throws IOException {
@@ -38,5 +56,9 @@ public class HttpClient {
 
     public int getResponseCode() {
         return responseCode;
+    }
+
+    public String getResponseHeader(String headerName) {
+        return responseHeaders.get(headerName);
     }
 }
