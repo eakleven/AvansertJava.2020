32caf3ef32932149509778c13b4233bb4fda7b56
eakleven <54510623+eakleven@users.noreply.github.com>
2020-09-09 13:04:05 +0200

    All tests green

--- src/main/java/no/kristiania/httpclient/QueryString.java
+++ src/main/java/no/kristiania/httpclient/QueryString.java
@@ -1,15 +1,26 @@
 package no.kristiania.httpclient;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class QueryString {
-    private final String value;
+    private Map<String, String> parameters = new HashMap<>();
+
 
     public QueryString(String queryString) {
-        int equalPos = queryString.indexOf('=');
-        value = queryString.substring(equalPos+1);
+        for (String parameter : queryString.split("&")){
+            int equalPos = parameter.indexOf('=');
+            String value = parameter.substring(equalPos+1);
+            String parameterName = parameter.substring(0, equalPos);
+            parameters.put(parameterName, value);
+        }
 
     }
 
     public String getParameter(String name) {
-        return value;
+        return parameters.get(name);
+
     }
+
 }
+
