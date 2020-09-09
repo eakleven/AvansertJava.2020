66ea450b84a27a43da02bc1817fdbb6ab96d0605
eakleven <54510623+eakleven@users.noreply.github.com>
2020-09-09 13:41:09 +0200

    Read success code - green Read failure code - red

---
+++ src/test/java/no/kristiania/httpclient/HttpClientTest.java
@@ -0,0 +1,19 @@
+package no.kristiania.httpclient;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HttpClientTest {
+    @Test
+    void shouldReadSuccessStatusCode(){
+        HttpClient httpClient = new HttpClient("urlecho.appspot.com", 80, "/echo?status=200");
+        assertEquals(200, httpClient.getResponseCode());
+    }
+    @Test
+    void shouldReadFailureStatusCode(){
+        HttpClient httpClient = new HttpClient("urlecho.appspot.com", 80, "/echo?status=200");
+        assertEquals(404, httpClient.getResponseCode());
+    }
+
+}
